30dd893396ac9ceb1f13768aa778fb388829b25e
albyabhi <127194147+albyabhi@users.noreply.github.com>
2023-03-23 02:23:26 -0400

    Add files via upload
    okeyy

--- Addstudents.jsx
+++ Addstudents.jsx
@@ -1,30 +1,43 @@
-import { Button, TextField, Typography } from '@mui/material'
+import { Button, TableContainer, TextField, Typography } from '@mui/material'
 import axios from 'axios'
 import React, { useState } from 'react'
 
-const Addstudents = () => {
-    var[addstudent,setaddstudent]=useState({
-   
-        id:"",
-        name:"",
-        grade:''
-    })
+const Addstudents = (props) => {
+    
+    var[addstudent,setaddstudent]=useState(props.data)
+    
     const handlechange=(e)=>{
         const{name,value}=e.target 
         setaddstudent({...addstudent,[name]:value})
         console.log(addstudent)
     }
     const savedata=()=>{
-        console.log("Button clicked")
-    
+      
+
+        if(props.method==="post"){
+            console.log("POST Button clicked")
     axios.post(" http://localhost:3005/students",addstudent)    
-    .then(Response=>{
+    .then(response=>{
         alert=("Success")
     })
     .catch(error=>{
         alert("failed")
     })
+
+}else if(props.method=="put"){
+    axios.put("http://localhost:3005/students/"+addstudent.id,addstudent)
+    .then((response)=>{
+        console.log("put data",response.data);
+    alert("success")
+        window.location.reload(false);
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
    }
+    }
+   
+
     
     
   return (
--- Read.jsx
+++ Read.jsx
@@ -1,8 +1,11 @@
 import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Addstudents from './Addstudents'
 
 const Read = () => {
+  var[update,setupdate]=useState(false)
+  var[singleValue,setSinglevalue]=useState([])
     var[students,setstudents]=useState([])
     useEffect(()=>{
         axios.get("http://localhost:3005/students")
@@ -11,7 +14,7 @@ const Read = () => {
             setstudents(students=response.data)
         })
         .catch(err=>console.log(err))
-    })
+    },[])
     const deletestudents =(id)=>{
       console.log("delete clicked");
       axios.delete("http://localhost:3005/students/"+id)
@@ -20,41 +23,51 @@ const Read = () => {
         window.location.reload(false)
       })
     }
+const updatestudents=(value)=>{
+  setSinglevalue(value);
+  setupdate(true);
 
+}
+var finalJSX =<TableContainer>
+<Table>
+    <TableHead>
+        <TableRow>
+        
+                
+                   <TableCell>id</TableCell>
+                    <TableCell>name</TableCell>
+                    <TableCell>grade</TableCell>
+                    <TableCell>delete</TableCell>
+            
+        </TableRow>
+    </TableHead>
+<TableBody>
+    {students.map((value,index)=>{
+        return<TableRow>
+            <TableCell>{value.id}</TableCell>
+            <TableCell>{value.name}</TableCell>
+            <TableCell>{value.grade}</TableCell>
+            <TableCell>
+              <Button onClick={()=>deletestudents(value.id)}>delete </Button>                   
+               </TableCell>
+               <TableCell>
+              <Button onClick={()=>updatestudents(value)}>update </Button>                   
+               </TableCell>
+        </TableRow>
+    })}
+</TableBody>
+
+
+
+</Table>
+</TableContainer>
+if(update)
+finalJSX = <Addstudents data={singleValue} method="put"/>
   return (
     
     <div>
-    
-      <TableContainer>
-        <Table>
-            <TableHead>
-                <TableRow>
-                
-                        
-                           <TableCell>id</TableCell>
-                            <TableCell>name</TableCell>
-                            <TableCell>grade</TableCell>
-                            <TableCell>delete</TableCell>
-                    
-                </TableRow>
-            </TableHead>
-        <TableBody>
-            {students.map((value,index)=>{
-                return<TableRow>
-                    <TableCell>{value.id}</TableCell>
-                    <TableCell>{value.name}</TableCell>
-                    <TableCell>{value.grade}</TableCell>
-                    <TableCell>
-                      <Button onClick={()=>deletestudents(value.id)}>delete </Button>                   
-                       </TableCell>
-                </TableRow>
-            })}
-        </TableBody>
-        
-        
-        
-        </Table>
-      </TableContainer>
+    {finalJSX}
+      
     </div>
   )
 }
